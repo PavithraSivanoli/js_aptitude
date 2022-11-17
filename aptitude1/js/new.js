fee078a076a91e1b77df32ad15553517890233ea
unknown <pavithrasivanoli@gmail.com>
2022-11-17 02:02:18 -0800

    Aptitude Program.

---
+++ aptitude1/js/new.js
@@ -0,0 +1,18 @@
+var bs=prompt("enter BS");
+var ss=prompt("enter SS");
+var upstreamSpeed;
+	bs=parseInt(bs);
+	ss=parseInt(ss);
+	upstreamSpeed=1/2*(bs-ss);
+	upstreamSpeed=parseInt(upstreamSpeed);
+	alert(upstreamSpeed);
+	
+	
+	var sb=prompt("enter SB");
+var sh=prompt("enter SH");
+var upstreamSpeed2;
+	sb=parseInt(sb);
+	sh=parseInt(sh);
+	upstreamSpeed2=1/2*(sb+sh);
+	upstreamSpeed2=parseInt(upstreamSpeed2);
+	alert(upstreamSpeed2);
