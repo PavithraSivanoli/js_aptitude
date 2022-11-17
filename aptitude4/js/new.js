8b9d2b1e37256e79900254c01383a863880c9a26
unknown <pavithrasivanoli@gmail.com>
2022-11-17 02:16:38 -0800

    Aptitude Program.

---
+++ aptitude4/js/new.js
@@ -0,0 +1,10 @@
+var pQ=prompt("enter pQ");
+var qR=prompt("enter qR");
+var Pr=prompt("enter Pr");
+var total;
+pQ=parseInt(pQ);
+qR=parseInt(qR);
+Pr=parseInt(Pr);
+total=pQ+qR+Pr;
+alert(total);
+
